df283ef8e42d2536c94e5a585dead6655a7a7406
Hugo Yang <hugoyang4567@gmail.com>
2025-02-28 15:06:48 -0500

    Added code to display integers without decimal points, cleaned up code

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,26 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  String _display = '0';
+  String _display = '';
   String _operand = '';
   double _secondOperand = 0;
   double _firstOperand = 0;
   String _prevOperand = '';
+  bool _decimal = false;
 
   void _onPressed(String value) {
     setState(() {
       // if user inputs a number or decimal value, appends to the value to be evaluated
       if ('0123456789.'.contains(value)) {
-        _display += value;
+        if(value == '.'){
+          if(!_decimal){
+            _display += value;
+            _decimal = true;
+          }
+        }
+        else{
+          _display += value;
+        }
         // if user inputs a operation, sets the current display value to first operand
         // _operand takes the operation to be made and the display is cleared
       } else if ('+-*/'.contains(value)) {
@@ -43,9 +52,10 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         _secondOperand = 0;
         _operand = value;
         _display = '';
+        _decimal = false;
         // if user inputs =, takes the current value and checks the operand
         // if operand is empty, check to see previous operand
-      } else if (value == '=') {
+      } else if (value == '=' && _display != '') {
         _secondOperand = _secondOperand == 0 ? double.parse(_display) : _secondOperand;
         double secondOperand = _secondOperand;
         if(_operand == ''){
@@ -53,35 +63,60 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           _prevOperand = '';
         }
         // switch conditions for the operations
+        // checks if the result is a integer, if yes converts to int
         switch (_operand) {
           case '+':
-            _display = (_firstOperand + secondOperand).toString();
-            _firstOperand = _firstOperand + secondOperand;
+            if((_firstOperand + secondOperand) % 1 != 0){
+              _display = (_firstOperand + secondOperand).toString();
+              _firstOperand = _firstOperand + secondOperand;
+            }
+            else{
+              _display = (_firstOperand + secondOperand).toInt().toString();
+              _firstOperand = _firstOperand + secondOperand;
+            }
             break;
           case '-':
-            _display = (_firstOperand - secondOperand).toString();
-            _firstOperand = _firstOperand - secondOperand;
-
+            if((_firstOperand - secondOperand) % 1 != 0){
+              _display = (_firstOperand - secondOperand).toString();
+              _firstOperand = _firstOperand - secondOperand;
+            }
+            else{
+              _display = (_firstOperand - secondOperand).toInt().toString();
+              _firstOperand = _firstOperand - secondOperand;
+            }
             break;
           case '*':
-            _display = (_firstOperand * secondOperand).toString();
-            _firstOperand = _firstOperand * secondOperand;
+           if((_firstOperand - secondOperand) % 1 != 0){
+              _display = (_firstOperand * secondOperand).toString();
+              _firstOperand = _firstOperand * secondOperand;
+            }
+            else{
+              _display = (_firstOperand * secondOperand).toInt().toString();
+              _firstOperand = _firstOperand * secondOperand;
+            }
 
             break;
           case '/':
+            if(secondOperand != 0 && ((_firstOperand / secondOperand) % 1 != 0)){            
             _display = secondOperand == 0 ? 'Error' : (_firstOperand / secondOperand).toString();
             _firstOperand = _firstOperand / secondOperand;
-
+            }
+            else{
+            _display = secondOperand == 0 ? 'Error' : (_firstOperand / secondOperand).toInt().toString();
+            _firstOperand = _firstOperand / secondOperand;
+            }
             break;
         }
         _prevOperand = _operand;
         _operand = '';
+        _decimal = false;
       // if user inputs 'C', clears and resets to beginning values
       } else if (value == 'C') {
-        _display = '0';
+        _display = '';
         _firstOperand = 0;
         _prevOperand = '';
         _operand = '';
+        _decimal = true;
       }
     });
   }
@@ -122,7 +157,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           Row(children: [_buildButton('7'), _buildButton('8'), _buildButton('9'), _buildButton('/')]),
           Row(children: [_buildButton('4'), _buildButton('5'), _buildButton('6'), _buildButton('*')]),
           Row(children: [_buildButton('1'), _buildButton('2'), _buildButton('3'), _buildButton('-')]),
-          Row(children: [_buildButton('0'), _buildButton('C'), _buildButton('+')]),
+          Row(children: [_buildButton('0'), _buildButton('.'), _buildButton('C'), _buildButton('+')]),
           Row(children: [_buildButton('=')]),
         ],
       ),
